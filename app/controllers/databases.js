88b0a2edc120c75da42690f8894cb8e8529833ba
monocle <hchw29@gmail.com>
2013-08-21 10:43:36 -0700

    Enable populating of an example db with Ember Github issues

--- app/controllers/databases.js
+++ app/controllers/databases.js
@@ -5,11 +5,39 @@ var DatabasesController = Ember.Controller.extend({
 
   selectDb: function() {
     var controller = this,
-        name = this.get('dbNameInput');
+        dbName = this.get('dbNameInput');
         
-    EIDB._open(name).then(function(db) {
+    EIDB._open(dbName).then(function(db) {
       controller.set('dbNameInput', null);
-      controller.transitionToRoute('stores.index', name);
+      controller.transitionToRoute('stores.index', dbName);
+    });
+  },
+
+  populateDb: function() {
+    var controller = this,
+        dbName = 'example_' + (new Date()).getTime(),
+        storeName = 'ember_github_issues';
+
+    Ember.RSVP.all(
+      [$.get("https://api.github.com/repos/emberjs/ember.js/issues?>page=1?per_page=30"),
+       $.get("https://api.github.com/repos/emberjs/ember.js/issues?>page=2?per_page=30")]
+    ).then(function(res){
+      var issues = [].concat.apply([], res);
+
+      EIDB._open(dbName, null, function(res) {
+        var db = res.db;
+        db.createObjectStore(storeName, {keyPath: 'id'});
+
+      }, true).then(function(db) {
+        issues.forEach(function(issue, i) {
+          db.add(storeName, i, issue);
+        });
+
+        return db;
+      }).then(function(db) {
+        db.close();
+        controller.transitionToRoute('stores.index', dbName);
+      });
     });
   }
 });
